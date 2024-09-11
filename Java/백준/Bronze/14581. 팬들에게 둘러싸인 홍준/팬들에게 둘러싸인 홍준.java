27ae6f3ddd30a516ee24b27e6eb83f13f0aef6b6
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2024-09-11 10:45:08 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 184 ms, Memory: 18280 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String hong = scan.nextLine();			
+		
+		scan.close();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + hong +"::fan:");
+		System.out.println(":fan::fan::fan:");
+		
+		
+	}
+}
