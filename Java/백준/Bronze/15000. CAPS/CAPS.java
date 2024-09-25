756cf64b8011834b772a01d75ce625098110a2cc
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2024-09-25 11:04:09 +0900

    [Bronze V] Title: CAPS, Time: 484 ms, Memory: 31624 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+
+        String res = st.toUpperCase();
+
+        System.out.println(res);
+        
+    }
+}
