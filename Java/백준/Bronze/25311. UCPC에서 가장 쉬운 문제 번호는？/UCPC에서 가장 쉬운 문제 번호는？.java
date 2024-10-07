aeba6be9a563886c4f069a0e557aa23ee3761284
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2024-10-07 11:45:08 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();       // y : UCPC 개최연도
+
+        sc.close();
+
+        System.out.println("A");
+
+    }
+}
