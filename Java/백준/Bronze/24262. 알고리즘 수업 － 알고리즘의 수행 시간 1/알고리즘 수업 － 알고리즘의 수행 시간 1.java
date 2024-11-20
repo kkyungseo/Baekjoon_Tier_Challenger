c63ba42abc321fdfe3ead59a371831eb4dc55cf7
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2024-11-20 09:43:15 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();     // n : 입력의 크기 (코드의 수행횟수)
+
+        System.out.println("1");
+        System.out.println("0");
+
+    }
+}
