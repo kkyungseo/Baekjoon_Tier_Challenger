0f710e5813ce70a4d6e49c729444bdb5f108fcec
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-03-01 18:10:21 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,13 @@
+while 1:
+    N = int(input())
+
+    if N == -1:
+        break;
+    num = []
+    for i in range(1, N):
+        if N % i == 0:
+            num.append(i)
+    if sum(num) == N:
+        print(N, " = ", " + ".join(str(i) for i in num), sep="")
+    else:
+        print(N, "is NOT perfect.")
