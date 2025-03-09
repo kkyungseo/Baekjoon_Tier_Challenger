e46e69625e9e3fbf4078eb88d94b924dc605acac
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-03-09 20:01:27 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,13 @@
+import sys
+
+# S : 입력받는 문자열 
+S = input()
+
+if S == "SONGDO":  # 비교 연산자로 수정
+    print("HIGHSCHOOL")
+elif S == "CODE":  # 비교 연산자로 수정
+    print("MASTER")
+elif S == "2023":  # 비교 연산자로 수정
+    print("0611")
+else:
+    print("CONTEST")
