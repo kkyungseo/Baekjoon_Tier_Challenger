2f36e1446844244bb46dc46326c3a5b5b231c143
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-03-11 12:54:19 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,15 @@
+import sys
+
+# S : 민재가 생각하는 동아리의 첫 번째 글자
+S = input()
+
+if S == "M" :
+    print("MatKor")
+elif S == "W" :
+    print("WiCys")
+elif S == "C" :
+    print("CyKor")
+elif S == "A" :
+    print("AlKor")
+else :
+    print("$clear")
