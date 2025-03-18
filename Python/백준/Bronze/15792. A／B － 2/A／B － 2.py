277c29a136b342b12ecc218c67b6e8e0344c9bbc
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-03-18 10:20:28 +0900

    [Bronze I] Title: A/B - 2, Time: 72 ms, Memory: 38372 KB, Score: 2000 point -BaekjoonHub

--- Python/백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ Python/백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,8 +1,14 @@
 import sys
+import decimal
 
-# A, B : 입력받는 두 정수 
-A, B = map(int, input().split())
+# 서브태스크의 정밀도를 충족하기 위해 코드 설정 (1000자리 이상 보장)
+# decimal로 임의 정밀도 연산
+decimal.getcontext().prec = 1001 
 
-result = A / B
+A, B = map(int, sys.stdin.readline().split())
 
+# Decimal을 사용하여 연산 수행
+result = decimal.Decimal(A) / decimal.Decimal(B)
+
+# 소수점 이하 1000자리까지 출력
 print(result)
