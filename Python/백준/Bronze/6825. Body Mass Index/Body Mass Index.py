eac2c75e19ec2530c0adc0fbd765ce72ee0d91c2
Kyung Seo KIM 👩‍💻 김경서 <biosnow19@sookmyung.ac.kr>
2025-04-05 18:22:59 +0900

    [Bronze IV] Title: Body Mass Index, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/6825. Body Mass Index/Body Mass Index.py
@@ -0,0 +1,13 @@
+import sys
+
+weight = float(input())
+height = float(input())
+
+bmi = weight / (height * height)
+
+if bmi > 25:
+    print("Overweight")
+elif 18.5 <= bmi <= 25.0:
+    print("Normal weight")
+else:
+    print("Underweight")
